4d8a0b0e30d84f3e3119f02b6cee6c728c1a89ab
lifelong-developer10 <shubhadahshingate@gmail.com>
2026-01-05 19:04:56 +0530

    Added given the tasks

---
+++ Task6-Calculator/script.js
@@ -0,0 +1,27 @@
+ 
+        function Add(){ 
+        let a =Number(document.getElementById("num1").value);
+        let b = Number(document.getElementById("num2").value);
+        document.getElementById("result").innerText = a+b;
+       
+        }
+         function Sub(){ 
+        let a =Number(document.getElementById("num1").value);
+        let b = Number(document.getElementById("num2").value);
+        document.getElementById("result").innerText = a-b;
+       
+        }
+         function Mul(){ 
+        let a =Number(document.getElementById("num1").value);
+        let b = Number(document.getElementById("num2").value);
+        document.getElementById("result").innerText = a*b;
+       
+        }
+         function Div(){ 
+        let a =Number(document.getElementById("num1").value);
+        let b = Number(document.getElementById("num2").value);
+        document.getElementById("result").innerText = a/b;
+       
+        }
+    
+    
